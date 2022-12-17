3b454411d2968c0919000b7fd973d3e22b63cdd5
josiebeepboop <josette.bitanga@gmail.com>
2022-12-16 22:32:16 -0500

    added summary tables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,10 @@
 library(dplyr)
 mechacar_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table)
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table))
+
+suspension_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+?summarize()
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # MechaCar_Statistical_Analysis
 
-##Linear Regression to Predict MPG
+## Purpose
+
+
+
+## Linear Regression to Predict MPG
+
+![lm-coefficient-mpg](Analysis/mpg-coefficient.png)
+
+We looked at the relationship of miles per gallon (mpg) versus vehicle length, vehicle weight, spoiler angle, ground clearance, and all wheel drive (awd) capabilities. Using R to plot the correlation between the various parameters, we can infer that the vehicle length, ground clearance, and awd are strongly correlated to mpg whereas vehicle weight and spoiler angle have a weak relationship with mpg. 
 
 ![lm-predict-mpg](Analysis/lm-predict-mpg.png)
+
